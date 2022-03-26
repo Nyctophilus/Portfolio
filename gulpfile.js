a54dda9b558c55c20a77163f3b7be2532902a55d
Medo <mohammed.yuossry@gmail.com>
2022-03-26 14:52:47 +0200

    added min img task with documented workflow for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,84 +1,114 @@
-const { src, dest, watch, series } = require("gulp");
-const dartSass = require("sass");
-const gulpSass = require("gulp-sass");
-const sass = gulpSass(dartSass);
-const postcss = require("gulp-postcss");
-const cssnano = require("cssnano");
-const autoprefixer = require("gulp-autoprefixer");
-const pug = require("gulp-pug");
-const concat = require("gulp-concat");
-const terser = require("gulp-terser");
-const browsersync = require("browser-sync").create();
+import gulp from "gulp";
+const { watch, series } = gulp;
+import gulpSass from "gulp-sass";
+import nodeSass from "node-sass";
+const sass = gulpSass(nodeSass);
+import postcss from "gulp-postcss";
+import cssnano from "cssnano";
+import gulpAutoprefixer from "gulp-autoprefixer";
+import pug from "gulp-pug";
+import concat from "gulp-concat";
+import terser from "gulp-terser";
+import BrowserSync from "browser-sync";
+const browsersync = BrowserSync.create();
+import imagemin from "gulp-imagemin";
+import webp from "gulp-webp";
 
 // Html Task
-function pugTask() {
-  return src("build/index.pug")
+gulp.task("pugTask", () => {
+  return gulp
+    .src("build/index.pug")
     .pipe(pug({ pretty: true }))
-    .pipe(dest("dist"));
-}
+    .pipe(gulp.dest("dist"));
+});
 
 // Sass Task
-function scssTask() {
-  return src("build/sass/**/*.scss", { sourcemaps: true })
+gulp.task("scssTask", () => {
+  return gulp
+    .src("build/sass/**/*.scss", { sourcemaps: true })
     .pipe(sass())
     .pipe(
-      autoprefixer({
+      gulpAutoprefixer({
         grid: "autoplace",
         AUTOPREFIXER_GRID: "autoplace",
       })
     )
     .pipe(postcss([cssnano()]))
     .pipe(concat("main.css"))
-    .pipe(dest("dist/css", { sourcemaps: "." }));
-}
+    .pipe(gulp.dest("dist/css", { sourcemaps: "." }));
+});
 
 // JavaScript Task
-function jsTask() {
-  return src("build/js/app.js", { sourcemaps: true })
+gulp.task("jsTask", () => {
+  return gulp
+    .src("build/js/app.js", { sourcemaps: true })
     .pipe(terser())
-    .pipe(dest("dist/js", { sourcemaps: "." }));
-}
+    .pipe(gulp.dest("dist/js", { sourcemaps: "." }));
+});
+
+// compress images Task
+gulp.task("imgTask", () => {
+  return gulp
+    .src("build/images/*.*")
+    .pipe(imagemin())
+    .pipe(webp())
+    .pipe(gulp.dest("dist/images"));
+});
+
+gulp.task("rootImgTask", () => {
+  return gulp
+    .src("build/preview.png")
+    .pipe(imagemin())
+    .pipe(webp())
+    .pipe(gulp.dest("dist/"));
+});
+
 // Browsersync Tasks
-function browsersyncServe(cb) {
+gulp.task("browsersyncServe", (cb) => {
   browsersync.init({
     server: {
       baseDir: "./dist",
     },
   });
   cb();
-}
+});
 
 // reload on changes
-function browsersyncReload(cb) {
+gulp.task("browsersyncReload", (cb) => {
   browsersync.reload();
   cb();
-}
+});
 
 // Watch task
-function watchTask() {
+gulp.task("watchTask", () => {
   // watch pug files
   watch(
     "build/**/*pug",
-    series(pugTask, browsersyncReload)
+    series("pugTask", "browsersyncReload")
   );
 
   //   watch scss files
   watch(
     "build/sass/**/*.scss",
-    series(scssTask, browsersyncReload)
+    series("scssTask", "browsersyncReload")
   );
 
   // watch js files
   watch(
     "build/js/**/*.js",
-    series(jsTask, browsersyncReload)
+    series("jsTask", "browsersyncReload")
   );
-}
 
-exports.default = series(
-  pugTask,
-  scssTask,
-  jsTask,
-  browsersyncServe,
-  watchTask
+  // watch Images
+  watch("build/images/**/*.*", "imgTask");
+  watch("build/preview.png", "rootImgTask");
+});
+
+export default series(
+  "pugTask",
+  "scssTask",
+  "jsTask",
+  "browsersyncServe",
+  "imgTask",
+  "watchTask"
 );
