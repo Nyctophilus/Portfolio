75ad0e367563c4b4a3bd277e4edcef9f96f202f2
Medo <mohammed.yuossry@gmail.com>
2022-03-28 16:25:48 +0200

    responsivity & functionalities for home&controllers
    globally adjusted fonts to be responsive at all devices' width
    
    made home section fully responsive at all devices
    
    added functionalities to the side controllers and light toggle button

--- build/js/app.js
+++ build/js/app.js
@@ -10,7 +10,7 @@
 	====================
 */
 
-const Toggle = () => {
+const lightToggle = () => {
   document.body.classList.toggle("light");
 };
 
@@ -26,6 +26,35 @@ const selectorG = (list) => {
   return Array.from(document.querySelectorAll(`${list}`));
 };
 
+const toggleActiveControllers = (con) => {
+  // remove active class from all controllers
+  selectorG(".controls .control").forEach((con) => {
+    con.classList.remove("active");
+  });
+
+  // add active to the clicked one!
+  con.classList.add("active");
+};
+
+const toggleActiveSections = (con) => {
+  // remove active class from all sections
+  selectorG("main > section").forEach((section) => {
+    section.classList.remove("active");
+  });
+
+  // add active to the clicked one!
+  selectorId(con.dataset.sect).classList.add("active");
+};
+
+const toggleBulbIcon = () => {
+  const icon = selectorUni(".light-toggler.control i.icon"),
+    cls = new RegExp(/^(icon-light)/);
+
+  if (cls.test(icon.classList[1])) {
+    icon.classList.toggle("icon-light");
+    icon.classList.toggle("icon-light1");
+  }
+};
 /*	
 	======================
 	==invoking functions==
@@ -35,16 +64,19 @@ const selectorG = (list) => {
 // execute light toggling!
 selectorId("light-toggler").addEventListener(
   "click",
-  Toggle
+  (e) => {
+    //  toggle bulb icon
+    toggleBulbIcon();
+
+    lightToggle();
+  }
 );
 
 // controllers
-selectorG(".controls .control").forEach((con) => {
-  con.addEventListener("click", () => {
-    selectorG(".controls .control").forEach((con) => {
-      con.classList.remove("active");
-    });
+selectorG(".controls .control").forEach((controller) => {
+  controller.addEventListener("click", () => {
+    toggleActiveControllers(controller);
 
-    selectorId(con.dataset.sect).classList.add("active");
+    toggleActiveSections(controller);
   });
 });
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,2 +1,83 @@
-const Toggle=()=>{document.body.classList.toggle("light")},selectorId=e=>document.getElementById(`${e}`),selectorUni=e=>document.querySelector(`${e}`),selectorG=e=>Array.from(document.querySelectorAll(`${e}`));selectorId("light-toggler").addEventListener("click",Toggle),selectorG(".controls .control").forEach((e=>{e.addEventListener("click",(()=>{selectorG(".controls .control").forEach((e=>{e.classList.remove("active")})),selectorId(e.dataset.sect).classList.add("active")}))}));
+/*	
+	=============
+	==Cash Selectors==
+	=============
+*/
+
+/*	
+	====================
+	==helper functions==
+	====================
+*/
+
+const lightToggle = () => {
+  document.body.classList.toggle("light");
+};
+
+const selectorId = (id) => {
+  return document.getElementById(`${id}`);
+};
+
+const selectorUni = (sel) => {
+  return document.querySelector(`${sel}`);
+};
+
+const selectorG = (list) => {
+  return Array.from(document.querySelectorAll(`${list}`));
+};
+
+const toggleActiveControllers = (con) => {
+  // remove active class from all controllers
+  selectorG(".controls .control").forEach((con) => {
+    con.classList.remove("active");
+  });
+
+  // add active to the clicked one!
+  con.classList.add("active");
+};
+
+const toggleActiveSections = (con) => {
+  // remove active class from all sections
+  selectorG("main > section").forEach((section) => {
+    section.classList.remove("active");
+  });
+
+  // add active to the clicked one!
+  selectorId(con.dataset.sect).classList.add("active");
+};
+
+const toggleBulbIcon = () => {
+  const icon = selectorUni(".light-toggler.control i.icon"),
+    cls = new RegExp(/^(icon-light)/);
+
+  if (cls.test(icon.classList[1])) {
+    icon.classList.toggle("icon-light");
+    icon.classList.toggle("icon-light1");
+  }
+};
+/*	
+	======================
+	==invoking functions==
+	======================
+*/
+
+// execute light toggling!
+selectorId("light-toggler").addEventListener(
+  "click",
+  (e) => {
+    //  toggle bulb icon
+    toggleBulbIcon();
+
+    lightToggle();
+  }
+);
+
+// controllers
+selectorG(".controls .control").forEach((controller) => {
+  controller.addEventListener("click", () => {
+    toggleActiveControllers(controller);
+
+    toggleActiveSections(controller);
+  });
+});
 //# sourceMappingURL=app.js.map
